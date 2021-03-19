8c5a507ea1948ee87a0c9606aabb636830c72f29
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-19 09:49:52 +0200

    writeFile

--- demo/fs.js
+++ demo/fs.js
@@ -1,10 +1,20 @@
 const fs = require("fs");
 const path = require("path");
 
-fs.mkdir(path.join(__dirname, "test"), (err) => {
+// fs.mkdir(path.join(__dirname, "test"), (err) => {
+//   if (err) {
+//     throw err;
+//   }
+
+//   console.log("Папка создана");
+// });
+
+const filePath = path.join(__dirname, "test", "text.txt");
+
+fs.writeFile(filePath, "Hello NodeJS!", (err) => {
   if (err) {
     throw err;
   }
 
-  console.log("Папка создана");
+  console.log("Файл создан");
 });
