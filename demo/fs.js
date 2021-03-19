8c027e0e5d92a3a3911732a3867a92adc3000fcb
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-19 09:44:55 +0200

    mkdir

--- demo/fs.js
+++ demo/fs.js
@@ -1,2 +1,10 @@
 const fs = require("fs");
 const path = require("path");
+
+fs.mkdir(path.join(__dirname, "test"), (err) => {
+  if (err) {
+    throw err;
+  }
+
+  console.log("Папка создана");
+});
