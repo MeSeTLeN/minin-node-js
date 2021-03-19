96325add0da4754ed45a33e0c53bbe8652bc6da3
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-19 09:24:29 +0200

    parse

--- demo/path.js
+++ demo/path.js
@@ -5,3 +5,5 @@ console.log("Название файла:", path.basename(__filename));
 console.log("Имя директории:", path.dirname(__filename));
 
 console.log("Расширение файла:", path.extname(__filename));
+
+console.log("Parse:", path.parse(__filename).name);
