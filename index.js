de6350640af12bf843df7eea83ad459ec2c4180a
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-19 11:07:11 +0200

    work with html

--- index.js
+++ index.js
@@ -1,14 +1,65 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "text/html",
-  });
-  //   res.writeHead(200, {
-  //     "Content-Type": "text/plain",
-  //   });
+  //   if (req.url === "/") {
+  //     fs.readFile(path.join(__dirname, "public", "index.html"), (err, data) => {
+  //       if (err) {
+  //         throw err;
+  //       }
 
-  res.end("<h1>Hello NodeJS!!!</h1>");
+  //       res.writeHead(200, {
+  //         "Content-Type": "text/html",
+  //       });
+  //       res.end(data);
+  //     });
+  //   } else {
+  //     fs.readFile(path.join(__dirname, "public", "contact.html"), (err, data) => {
+  //       if (err) {
+  //         throw err;
+  //       }
+
+  //       res.writeHead(200, {
+  //         "Content-Type": "text/html",
+  //       });
+  //       res.end(data);
+  //     });
+  //   }
+
+  let filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "index.html" : req.url
+  );
+  const ext = path.extname(filePath);
+
+  if (!ext) {
+    filePath += ".html";
+  }
+
+  console.log(filePath);
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      fs.readFile(path.join(__dirname, "public", "error.html"), (err, data) => {
+        if (err) {
+          res.writeHead(500);
+          res();
+        } else {
+          res.writeHead(200, {
+            "Content-Type": "text/html",
+          });
+          res.end(data);
+        }
+      });
+    } else {
+      res.writeHead(200, {
+        "Content-Type": "text/html",
+      });
+      res.end(content);
+    }
+  });
 });
 
 server.listen(3000, () => {
