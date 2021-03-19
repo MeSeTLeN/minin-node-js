00278a3853feeb2cc55dbaf99f880de8b9b97e4c
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-19 10:24:31 +0200

    on, emit

--- demo/events.js
+++ demo/events.js
@@ -1,1 +1,14 @@
 const EventEmitter = require("events");
+
+const emitter = new EventEmitter();
+
+emitter.on("anything", (data) => {
+  console.log("ON: anything", data);
+});
+
+emitter.emit("anything", { a: 1 });
+emitter.emit("anything", { b: 2 });
+
+setTimeout(() => {
+  emitter.emit("anything", { c: 3 });
+}, 1500);
