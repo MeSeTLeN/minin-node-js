68f94a2ced1ebcf713a3ef0714727adcd1e69801
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-19 09:23:52 +0200

    extname

--- demo/path.js
+++ demo/path.js
@@ -3,3 +3,5 @@ const path = require("path");
 console.log("Название файла:", path.basename(__filename));
 
 console.log("Имя директории:", path.dirname(__filename));
+
+console.log("Расширение файла:", path.extname(__filename));
