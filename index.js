3c2c50d7819037e5fff78eab3ac09a781775064e
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-19 10:49:56 +0200

    writeHead

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "text/html",
+  });
+  //   res.writeHead(200, {
+  //     "Content-Type": "text/plain",
+  //   });
+
   res.end("<h1>Hello NodeJS!!!</h1>");
 });
 
